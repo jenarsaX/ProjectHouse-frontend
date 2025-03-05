99c8412fb7596a63dd213a62206c4a81aa6e3820
jenarsaX <u21316662@utp.edu.pe>
2025-03-05 12:53:42 -0500

    DeparmentModel

---
+++ src/app/Model/Department.ts
@@ -0,0 +1,10 @@
+export interface Deparment {
+    id? : string;
+    name?: string;
+    tamaño?: string;
+    precio? : string;
+    estatus?: string;
+    description?: string;
+    fecha_agregado?: string;
+    pisos?: string;
+}
