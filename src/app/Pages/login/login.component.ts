97b7734e30dd218a793cf4dbd2498491c0333779
jenarsaX <u21316662@utp.edu.pe>
2025-03-05 12:32:48 -0500

    Auth y Login

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../Services/ServiceAuth/auth.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+
+export class LoginComponent implements OnInit{
+    loginform: FormGroup;
+    errorMessage: string | null = null;
+    isloading: boolean = false;
+    verpassword : boolean = false;
+
+  togglePasswordVisibility(): void{
+    this.verpassword = !this.verpassword;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginform = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+
+    ngOnInit(): void {
+      throw new Error('Method not implemented.');
+    }
 }
